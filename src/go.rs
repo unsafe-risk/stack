6dcfe493af9e5cae414569996ffce2d60d78783b
snowmerak <snowmerak@outlook.com>
2024-02-18 14:15:10 +0900

    Refactor Go.rs to use async/await for file operations, aiding readability.

--- src/go.rs
+++ src/go.rs
@@ -1,16 +1,9 @@
+use std::error::Error;
 
-pub const DEPENDENCY_KEY_TEMPLATE : &str = "package {}
+use tokio::{fs::File, io::AsyncWriteExt, process::Command};
 
-const dependencyKey = `{}`
+use crate::category::Category;
 
-func Dependency() string {
-    return dependencyKey
-}
-
-func ({} *{}) Dependency() string {
-    return dependencyKey
-}
-";
 
 pub const PROVIDER_CODE : &str = "package provider
 
@@ -24,7 +17,7 @@ import (
 type ErrNotAFunction struct{}
 
 func (e ErrNotAFunction) Error() string {
-	return "not a function"
+	return \"not a function\"
 }
 
 func analyzeConstructor(constructFunction any) ([]reflect.Type, []reflect.Type, error) {
@@ -99,7 +92,7 @@ func Get[T any](provider *Provider) (T, bool) {
 type ErrInvalidFunctionReturn struct{}
 
 func (e ErrInvalidFunctionReturn) Error() string {
-	return "invalid function return"
+	return \"invalid function return\"
 }
 
 func Run[T any](provider *Provider, function any) (r T, err error) {
@@ -111,7 +104,7 @@ func Run[T any](provider *Provider, function any) (r T, err error) {
 		return r, err
 	}
 
-	if len(rets) != 2 || rets[1].String() != "error" || rets[0] != reflect.TypeOf(*new(T)) {
+	if len(rets) != 2 || rets[1].String() != \"error\" || rets[0] != reflect.TypeOf(*new(T)) {
 		return r, ErrInvalidFunctionReturn{}
 	}
 
@@ -142,7 +135,7 @@ func JustRun(provider *Provider, function any) error {
 		return err
 	}
 
-	if len(rets) != 1 || rets[0].String() != "error" {
+	if len(rets) != 1 || rets[0].String() != \"error\" {
 		return ErrInvalidFunctionReturn{}
 	}
 
@@ -196,13 +189,13 @@ type ErrNotProvided struct {
 }
 
 func (e ErrNotProvided) Error() string {
-	return "not provided: " + e.Type.String()
+	return \"not provided: \" + e.Type.String()
 }
 
 type ErrInvalidConstructorReturn struct{}
 
 func (e ErrInvalidConstructorReturn) Error() string {
-	return "invalid constructor return"
+	return \"invalid constructor return\"
 }
 
 type ErrMaybeCyclicDependency struct {
@@ -211,11 +204,11 @@ type ErrMaybeCyclicDependency struct {
 
 func (e ErrMaybeCyclicDependency) Error() string {
 	sb := strings.Builder{}
-	sb.WriteString("maybe cyclic dependency: ")
+	sb.WriteString(\"maybe cyclic dependency: \")
 	for i, con := range e.cons {
 		sb.WriteString(con.String())
 		if i != len(e.cons)-1 {
-			sb.WriteString(", ")
+			sb.WriteString(\", \")
 		}
 	}
 
@@ -249,7 +242,7 @@ func (p *Provider) Construct(ctx context.Context) error {
 				returns := con.Call(args)
 
 				for _, ret := range returns {
-					if ret.Type().Kind().String() == "error" {
+					if ret.Type().Kind().String() == \"error\" {
 						if !ret.IsNil() {
 							return ret.Interface().(error)
 						}
@@ -277,12 +270,103 @@ func (p *Provider) Construct(ctx context.Context) error {
 }
 ";
 
-pub const STRUCTURE_TEMPLATE : &str = "package {}
+pub async fn write_struct(writer: &mut File, name: &str) -> Result<(), Box<dyn Error>> {
+	writer.write(format!("package {}
+	
+type {} struct {{
+}}
 
-type {} struct {
+func New() *{} {{
+	return &{}{{}}
+}}
+
+", name, name, name, name).as_bytes()).await?;
+
+	Ok(())
 }
 
-func New{}() *{} {
-    return &{}
+pub async fn write_interface(writer: &mut File, name: &str) -> Result<(), Box<dyn Error>> {
+	writer.write(format!("package {}
+
+type {} interface {{
+}}
+
+func Check(i {}) {{}}
+
+", name, name, name).as_bytes()).await?;
+
+	Ok(())
 }
-";
+
+const SERVICE_FOLDER : &str = "./internal/service";
+const MODEL_FOLDER : &str = "./lib/model";
+const CONTRACT_FOLDER : &str = "./lib/contract";
+const MEDIATOR_FOLDER : &str = "./internal/mediator";
+const AGGREGATOR_FOLDER : &str = "./internal/aggregator";
+const HANDLER_FOLDER : &str = "./internal/handler";
+const ADAPTER_FOLDER : &str = "./internal/adapter";
+const SERVER_FOLDER : &str = "./internal/server";
+const MESSAGE_FOLDER : &str = "./lib/message";
+const PROTOCOL_FOLDER : &str = "./lib/protocol";
+const STATE_FOLDER : &str = "./lib/state";
+
+pub async fn init_go_mod(name: &str) -> Result<(), Box<dyn Error>> {
+	let output = Command::new("go")
+		.arg("mod")
+		.arg("init")
+		.arg(name)
+		.output().await?;
+
+	if !output.status.success() {
+		return Err("Failed to initialize go mod".into());
+	}
+
+	let provider_path = "gen/provider";
+
+	tokio::fs::create_dir_all(provider_path).await?;
+	tokio::fs::create_dir_all(SERVICE_FOLDER).await?;
+	tokio::fs::create_dir_all(MODEL_FOLDER).await?;
+	tokio::fs::create_dir_all(CONTRACT_FOLDER).await?;
+	tokio::fs::create_dir_all(MEDIATOR_FOLDER).await?;
+	tokio::fs::create_dir_all(AGGREGATOR_FOLDER).await?;
+	tokio::fs::create_dir_all(HANDLER_FOLDER).await?;
+	tokio::fs::create_dir_all(ADAPTER_FOLDER).await?;
+	tokio::fs::create_dir_all(SERVER_FOLDER).await?;
+	tokio::fs::create_dir_all(MESSAGE_FOLDER).await?;
+	tokio::fs::create_dir_all(PROTOCOL_FOLDER).await?;
+	// tokio::fs::create_dir_all(STATE_FOLDER).await?;
+	
+	let mut provider_file = File::create(format!("{}/provider.go", provider_path)).await?;
+
+	provider_file.write(PROVIDER_CODE.as_bytes()).await?;
+
+	Ok(())
+}
+
+pub async fn generate_file(name: &str, path: &str, category: &Category) -> Result<(), Box<dyn Error>> {
+	let lower_case_name = name.to_lowercase();
+	let file = match category {
+		Category::Service => format!("{}/{}/{}", SERVICE_FOLDER, path, lower_case_name),
+		Category::Model => format!("{}/{}/{}", MODEL_FOLDER, path, lower_case_name),
+		Category::Contract => format!("{}/{}/{}", CONTRACT_FOLDER, path, lower_case_name),
+		Category::Mediator => format!("{}/{}/{}", MEDIATOR_FOLDER, path, lower_case_name),
+		Category::Aggregator => format!("{}/{}/{}", AGGREGATOR_FOLDER, path, lower_case_name),
+		Category::Handler => format!("{}/{}/{}", HANDLER_FOLDER, path, lower_case_name),
+		Category::Adapter => format!("{}/{}/{}", ADAPTER_FOLDER, path, lower_case_name),
+		Category::Server => format!("{}/{}/{}", SERVER_FOLDER, path, lower_case_name),
+		Category::Message => format!("{}/{}/{}", MESSAGE_FOLDER, path, lower_case_name),
+		Category::Protocol => format!("{}/{}/{}", PROTOCOL_FOLDER, path, lower_case_name),
+		Category::State => format!("{}/{}/{}", STATE_FOLDER, path, lower_case_name),
+	};
+
+	tokio::fs::create_dir_all(&file).await?;
+
+	let mut file = File::create(format!("{}/{}.go", file, lower_case_name)).await?;
+
+	match category {
+		Category::Contract => write_interface(&mut file, name).await?,
+		_ => write_struct(&mut file, name).await?,
+	};
+
+	Ok(())
+}
