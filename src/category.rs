ce04c09b9a8f448afc51c73d4e29b14bbb308a91
snowmerak <snowmerak@outlook.com>
2024-02-18 14:15:28 +0900

    Add a new `Category` enum to src/category.rs for better organization and readability.

---
+++ src/category.rs
@@ -0,0 +1,35 @@
+use std::fmt::Display;
+
+
+#[derive(Debug, PartialEq)]
+pub enum Category {
+    Service,
+    Mediator,
+    Aggregator,
+    Handler,
+    Adapter,
+    Server,
+    Protocol,
+    Message,
+    State,
+    Contract,
+    Model,
+}
+
+impl Display for Category {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Category::Service => write!(f, "service"),
+            Category::Mediator => write!(f, "mediator"),
+            Category::Aggregator => write!(f, "aggregator"),
+            Category::Handler => write!(f, "handler"),
+            Category::Adapter => write!(f, "adapter"),
+            Category::Server => write!(f, "server"),
+            Category::Protocol => write!(f, "protocol"),
+            Category::Message => write!(f, "message"),
+            Category::State => write!(f, "state"),
+            Category::Contract => write!(f, "contract"),
+            Category::Model => write!(f, "model"),
+        }
+    }
+}
